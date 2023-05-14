1d4dbe85de71764e0a2b5c57adac7cd77ca309ca
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-05-13 19:47:56 -0500

    working on api routes

---
+++ assets/routing/api-routing.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+var uniqid = require('uniqid');
+
+// routing
+module.exports = function (app){
+    app.get('./notes', (req, res) => {
+        console.log('notes app working');
+
+        let data = fs.readFileSync('./assets/db/db.json', 'utf8')
+
+        res.json(JSON.parse(data));
+    })
+    app.post('/api/notes', (req, res) => {
+        const newNote = {
+            ...req.body,
+            id: uniqid(),
+        };
+
+    let data = fs.readFileSync('./assets/db/db.json', 'utf8')
+    
+    const dataJSON = JSON.parse(data);
+
+    dataJSON.push(newNote);
+    
+
+    fs.writeFile(
+        './assets/db/db.json',
+        JSON.stringify(dataJSON),
+        (err, text) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        }
+    );
+    console.log('done', text)
+    res.json(data);
+});
+    app.delete('./notes/:id', (req,res) => {
+        let data = fs.readFileSync('assets/db/db.json', 'utf8');
+
+        const dataJSON = JSON.parse(data);
+
+        const newNotes = dataJSON.filter((note) => {
+            return note.id !== req.params.id;
+        });
+
+        fs.writeFile('./assets/db/db.json', Json.stringify(newNotes),(err,text) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+        })
+        res.json(newNotes);
+    });
+};
