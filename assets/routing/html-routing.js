7872721b732a167ec5dd4047249d2444ee5f23a3
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-05-14 17:17:48 -0500

    working on routing

--- assets/routing/html-routing.js
+++ assets/routing/html-routing.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + './notes.html'))
     });
 
-    app.get('/', function(req, res){
+    app.get('/notes', function(req, res){
         res.sendFile(path.join(__dirname + './index.html'))
     });
 }
